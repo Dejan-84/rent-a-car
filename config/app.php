1095e69853fff963a7c722fbd3889869fe993799
Dejan-84 <dekidjurdjev@gmail.com>
2024-03-25 13:29:12 +0100

    new

--- config/app.php
+++ config/app.php
@@ -1,8 +1,20 @@
 <?php
 
-define('DB_SERVER', 'dejandjurdjev.com');
-define('DB_USER', 'u921082012_u921082012_u93');
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'u724976138_user1');
 define('DB_PASSWORD', '#Dejan1984');
-define('DB_NAME', 'u921082012_objektno');
+define('DB_NAME', 'u724976138_furniture');
+
+define('ENVIRONMENT', 'production');
+
+
+/*define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'u724976138_user1');
+define('DB_PASSWORD', '#Dejan1984');
+define('DB_NAME', 'u724976138_furniture');
+
+define('ENVIRONMENT', 'production');*/
+
+
 
 ?>
