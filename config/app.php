8cc79fed14117cfd74e7017777563f9870b6000f
Dejan-84 <dekidjurdjev@gmail.com>
2024-03-27 11:19:40 +0100

    new db

--- config/app.php
+++ config/app.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'u724976138_user1');
+define('DB_USER', 'u724976138_user2');
 define('DB_PASSWORD', '#Dejan1984');
-define('DB_NAME', 'u724976138_furniture');
+define('DB_NAME', 'u724976138_rentacar');
 
 define('ENVIRONMENT', 'production');
 
